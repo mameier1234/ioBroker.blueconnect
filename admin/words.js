2dd7d6379d7a63b2e7ceaa160dd7fca3753e7e93
Martin <martin@iobroker-dev.localdomain>
2020-07-26 21:17:02 +0200

    admin

--- admin/words.js
+++ admin/words.js
@@ -14,28 +14,20 @@ systemDictionary = {
         'pl': 'Ustawienia adaptera dla template',
         'zh-cn': 'template的适配器设置'
     },
-    'option1': {
-        'en': 'option1',
-        'de': 'Option 1',
-        'ru': 'Опция 1',
-        'pt': 'Opção 1',
-        'nl': 'Optie 1',
-        'fr': 'Option 1',
-        'it': 'opzione 1',
-        'es': 'Opción 1',
-        'pl': 'opcja 1',
-        'zh-cn': '选项1'
+    'email': {
+        'en': 'eMail-Adress',
+        'de': 'eMail-Adresse',
     },
-    'option2': {
-        'en': 'option2',
-        'de': 'Option 2',
-        'ru': 'option2',
-        'pt': 'opção 2',
-        'nl': 'Optie 2',
-        'fr': 'Option 2',
-        'it': 'opzione 2',
-        'es': 'opcion 2',
-        'pl': 'Opcja 2',
-        'zh-cn': '选项2'
+    'password': {
+        'en': 'Password',
+        'de': 'Passwort',
+    },
+    'poolID': {
+        'en': 'Pool ID',
+        'de': 'Pool ID',
+    },
+    'blueConnectKey': {
+        'en': 'BlueConnect Key',
+        'de': 'BlueConnect Key',
     }
 };
