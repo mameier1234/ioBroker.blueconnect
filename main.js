e1941b44e64982cbd6283036844dbcfcd85a7a17
kopierschnitte <chris@bispingweb.de>
2021-05-30 11:47:34 +0200

    Update main.js
    Iterate every pool and its sensors to get the adapter working with just username/password.

--- main.js
+++ main.js
@@ -21,8 +21,6 @@ class Blueconnect extends utils.Adapter {
         
         var email = bc.config.email;
         var password = bc.config.password;
-        var poolID = bc.config.poolID;
-        var blueConnectKey = bc.config.blueConnectKey;
         
         function createObj(id, name, type) {
             bc.setObjectNotExists(id, {
@@ -42,10 +40,13 @@ class Blueconnect extends utils.Adapter {
             bc.setState(id, {val: value, ack: true});
         }
         
-        function process(key,value, index) {
-            if(index!=='') {
+        function process(key,value, index, device) {
+            if(index!=='') {
                 key = index + "." + key;
             }
+	    if(device!=='') {
+    		key = device + "." + key;
+	    }
             createObj(key, key, typeof(value));
             setValue(key, value);
         }
@@ -70,10 +71,11 @@ class Blueconnect extends utils.Adapter {
         }
 
         
-        function getMeasurements(access_key, secret_key, session_token) {
+        function getMeasurements(access_key, secret_key, session_token, poolID, blueConnectKey) {
             var myMethod = 'GET';
             var myPath = '/prod/swimming_pool/' + poolID + '/blue/' + blueConnectKey + '/lastMeasurements';
-            var region = 'eu-west-1';
+            //var myPath = '/prod/swimming_pool/' + poolID + '/blue';
+	    var region = 'eu-west-1';
             var myService = 'execute-api';
             
             var amzDate = getAmzDate(new Date().toISOString());
@@ -114,7 +116,7 @@ class Blueconnect extends utils.Adapter {
 
             var requestOptions = {
                 'method': 'GET',
-                'uri': 'https://api.riiotlabs.com/prod/swimming_pool/' + poolID + '/blue/' + blueConnectKey + '/lastMeasurements?mode=blue_and_strip',
+		'uri': 'https://api.riiotlabs.com/prod/swimming_pool/' + poolID + '/blue/' + blueConnectKey + '/lastMeasurements?mode=blue_and_strip',
                 headers: {
                     'accept-encoding': 'gzip;q=1.0,compress;q=0.5',
                     'accept-language': 'de-DE;q=1.0',
@@ -128,9 +130,11 @@ class Blueconnect extends utils.Adapter {
 
             request.get(requestOptions, function(error, response, body) {
                 var result = JSON.parse(body);
+                //bc.log.debug(JSON.stringify(body));
+
                 for(var el in result) {
                     if(typeof(result[el])=="string") {
-                        process(el, result[el], "");
+                        process(el, result[el], "", poolID);
                     }
                 }
 
@@ -138,13 +142,169 @@ class Blueconnect extends utils.Adapter {
                     createObj(result["data"][el]["name"], result["data"][el]["name"], "");
                     for(var value in result["data"][el]) {
                         if(typeof(result["data"][el][value])!=="object") {
-                            process(value, result["data"][el][value], result["data"][el]["name"]);
+                            process(value, result["data"][el][value], result["data"][el]["name"], poolID);
                         }
                     }
                 }
 
             });
         }
+
+	function getPool(access_key, secret_key, session_token) {
+	    var poolIDs = [];
+            var myMethod = 'GET';
+            var myPath = '/prod/swimming_pool'; //swimming_pool
+            var region = 'eu-west-1';
+            var myService = 'execute-api';
+            
+            var amzDate = getAmzDate(new Date().toISOString());
+            var authDate = amzDate.split("T")[0];
+            var payload = '';
+            var hashedPayload = crypto.SHA256(payload).toString();
+
+            var canonicalReq =  myMethod + '\n' +
+                                myPath + '\n' +
+                                'deleted=false\n' +
+                                'accept-encoding:gzip;q=1.0,compress;q=0.5\n' +
+                                'accept-language:de-DE;q=1.0\n' +
+                                'host:api.riiotlabs.com\n' +
+                                'user-agent:aws-sdk-iOS/2.4.6 iOS/13.1 de_DE\n' +
+                                'x-amz-date:' + amzDate + '\n' +
+                                'x-amz-security-token:' + session_token + '\n' + 
+                                '\n' +
+                                'accept-encoding;accept-language;host;user-agent;x-amz-date;x-amz-security-token' + '\n' +
+                                hashedPayload;
+
+            var canonicalReqHash = crypto.SHA256(canonicalReq).toString();
+            var stringToSign =  'AWS4-HMAC-SHA256\n' +
+                                amzDate + '\n' +
+                                authDate+'/'+region+'/'+myService+'/aws4_request\n'+
+                                canonicalReqHash;
+
+            var signingKey = getSignatureKey(crypto, secret_key, authDate, region, myService);
+            var authKey = crypto.HmacSHA256(stringToSign, signingKey);
+
+            var authString  = 'AWS4-HMAC-SHA256 ' +
+                              'Credential='+
+                              access_key+'/'+
+                              authDate+'/'+
+                              region+'/'+
+                              myService+'/aws4_request,'+
+                              'SignedHeaders=accept-encoding;accept-language;host;user-agent;x-amz-date;x-amz-security-token '+
+                              'Signature='+authKey;
+
+            var requestOptions = {
+                'method': 'GET',
+                'uri': 'https://api.riiotlabs.com/prod/swimming_pool?deleted=false',
+                headers: {
+                    'accept-encoding': 'gzip;q=1.0,compress;q=0.5',
+                    'accept-language': 'de-DE;q=1.0',
+                    'host': 'api.riiotlabs.com',
+                    'user-agent': 'aws-sdk-iOS/2.4.6 iOS/13.1 de_DE',
+                    'x-amz-date': amzDate,
+                    'x-amz-security-token': session_token,
+                    'authorization': authString
+                }
+            };
+
+            request.get(requestOptions, function(error, response, body) {
+
+                var result = JSON.parse(body);
+
+               
+                for(var el in result["data"]) {
+                    for(var value in result["data"][el]) {
+                        if(typeof(result["data"][el][value])!=="object") {                        
+			    if(value=="swimming_pool_id") {
+				getBlue(access_key, secret_key, session_token, result["data"][el][value]);
+			    }
+                        }
+                    }
+                }
+
+            
+            });
+            
+        }
+
+	function getBlue(access_key, secret_key, session_token, poolID) {
+	    
+            var myMethod = 'GET';
+            var myPath = '/prod/swimming_pool/' + poolID + '/blue';
+            var region = 'eu-west-1';
+            var myService = 'execute-api';
+            
+            var amzDate = getAmzDate(new Date().toISOString());
+            var authDate = amzDate.split("T")[0];
+            var payload = '';
+            var hashedPayload = crypto.SHA256(payload).toString();
+
+            var canonicalReq =  myMethod + '\n' +
+                                myPath + '\n' +
+                                'deleted=false\n' +
+                                'accept-encoding:gzip;q=1.0,compress;q=0.5\n' +
+                                'accept-language:de-DE;q=1.0\n' +
+                                'host:api.riiotlabs.com\n' +
+                                'user-agent:aws-sdk-iOS/2.4.6 iOS/13.1 de_DE\n' +
+                                'x-amz-date:' + amzDate + '\n' +
+                                'x-amz-security-token:' + session_token + '\n' + 
+                                '\n' +
+                                'accept-encoding;accept-language;host;user-agent;x-amz-date;x-amz-security-token' + '\n' +
+                                hashedPayload;
+
+            var canonicalReqHash = crypto.SHA256(canonicalReq).toString();
+            var stringToSign =  'AWS4-HMAC-SHA256\n' +
+                                amzDate + '\n' +
+                                authDate+'/'+region+'/'+myService+'/aws4_request\n'+
+                                canonicalReqHash;
+
+            var signingKey = getSignatureKey(crypto, secret_key, authDate, region, myService);
+            var authKey = crypto.HmacSHA256(stringToSign, signingKey);
+
+            var authString  = 'AWS4-HMAC-SHA256 ' +
+                              'Credential='+
+                              access_key+'/'+
+                              authDate+'/'+
+                              region+'/'+
+                              myService+'/aws4_request,'+
+                              'SignedHeaders=accept-encoding;accept-language;host;user-agent;x-amz-date;x-amz-security-token '+
+                              'Signature='+authKey;
+
+            var requestOptions = {
+                'method': 'GET',
+                'uri': 'https://api.riiotlabs.com/prod/swimming_pool/' + poolID + '/blue?deleted=false',
+                headers: {
+                    'accept-encoding': 'gzip;q=1.0,compress;q=0.5',
+                    'accept-language': 'de-DE;q=1.0',
+                    'host': 'api.riiotlabs.com',
+                    'user-agent': 'aws-sdk-iOS/2.4.6 iOS/13.1 de_DE',
+                    'x-amz-date': amzDate,
+                    'x-amz-security-token': session_token,
+                    'authorization': authString
+                }
+            };
+
+            request.get(requestOptions, function(error, response, body) {
+
+                var result = JSON.parse(body);
+
+               
+                for(var el in result["data"]) {
+                    for(var value in result["data"][el]) {
+                        if(typeof(result["data"][el][value])!=="object") {                        
+			    if(value=="blue_device_serial") {
+                                getMeasurements(access_key, secret_key, session_token, poolID, result["data"][el][value]);
+			    }
+                        }
+                    }
+                }
+
+            
+            });
+            
+        }
+
+
         
         request.post({
             method: 'POST',
@@ -162,7 +322,7 @@ class Blueconnect extends utils.Adapter {
             var access_key = body.credentials.access_key;
             var secret_key = body.credentials.secret_key;
             var session_token = body.credentials.session_token;
-            getMeasurements(access_key, secret_key, session_token);
+	    getPool(access_key, secret_key, session_token);
         });
                 
         this.subscribeStates('*');
